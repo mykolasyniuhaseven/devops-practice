30fa0e6031100f7517072fd435132fb897069c78
mykolasyniuhaseven <mykola.synuiha@true-skin.de>
2023-10-16 13:22:33 +0300

    deploy test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World777!');
+    it('should return "Hello World777"', () => {
+      expect(appController.getHello()).toBe('Hello World777');
     });
   });
 });
